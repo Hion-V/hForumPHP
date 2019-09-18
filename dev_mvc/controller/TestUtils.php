e549b31549a23a7a30f0690f62b87de31ef9b313
Andreas <the.hion.v@gmail.com>
2019-09-18 17:00:36 +0200

    Created testutils class

---
+++ dev_mvc/controller/TestUtils.php
@@ -0,0 +1,13 @@
+<?php
+
+
+class TestUtils{
+    public static $log = [];
+    public static $status;
+    public static function log($output, $status = "OK"){
+        array_push(self::log, ["message" => $output, "status" => $status]);
+    }
+    public static function returnLog(){
+        echo(json_encode(self::$log));
+    }
+}
