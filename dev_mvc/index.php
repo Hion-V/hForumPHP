967876dbab4879209732054d9cd9abb7723f104b
Andreas <the.hion.v@gmail.com>
2019-09-19 12:09:43 +0200

    removed reference to nonexistant file

--- dev_mvc/index.php
+++ dev_mvc/index.php
@@ -1,5 +1,4 @@
 <?php
-require_once('./controller/TestUtils.php');
 //date_default_timezone_set('Europe/Amsterdam');
 require_once('./controller/MVCController.php');
 require_once('./controller/UserSession.php');
