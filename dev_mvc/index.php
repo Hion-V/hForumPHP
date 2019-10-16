c8b5d55953dbcd7b34d23f85568aeea04b72faef
Andreas Schaafsma <the.hion.v@gmail.com>
2019-10-16 10:39:34 +0200

    added redis key and code to keep track if db exist

--- dev_mvc/index.php
+++ dev_mvc/index.php
@@ -22,6 +22,7 @@ use model\testactions\TestAction;
 use controller\MVCController;
 use controller\UserSession;
 use controller\HUtils;
+use controller\db\Database;
 
 
 
@@ -33,6 +34,17 @@ ini_set('log_errors','1');
 ini_set('display_errors','1');
 
 session_start();
+
+$redis = new Redis();
+$redis->connect('sc-redis','6379');
+$redis->auth("password");
+$redis->set('DB_CREATED', false);
+echo $redis->get('DB_CREATED');
+if(!$redis->get('DB_CREATED') || $redis->get('DB_CREATED') == ''){
+    Database::createDBIfNotPresent();
+    $redis->set('DB_CREATED', true);
+}
+
 
 //date_default_timezone_set('Europe/Amsterdam');
 
